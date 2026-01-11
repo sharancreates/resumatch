f06e2761c9a6c11e2e464bd16b3ff3df7b1f848a
sharancreates <sharanyanagar@yahoo.in>
2026-01-11 11:41:40 +0530

    Force downgrade to Tailwind v3

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,7 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: 'class', // <--- This is the magic line
+  darkMode: 'class',
   theme: {
     extend: {},
   },
